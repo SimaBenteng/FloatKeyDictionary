2a9944057933d2a099c033eb04f6aeeb277f647b
司马奔腾 <simabenteng@qq.com>
2019-02-18 17:15:57 +0800

    fix type test bug

--- FloatKeyDictionary.js
+++ FloatKeyDictionary.js
@@ -75,7 +75,7 @@ class FloatKeyDictionary extends Map
 					for (let i = 0; i < keys.length; i ++)
 					{
 						const k = keys[i];
-						if (typeof k !== 'number')
+						if (isNaN(parseFloat(k)))
 						{
 							throw new Error(`key -> ${k}不为number类型。`);
 						}
